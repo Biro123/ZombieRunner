0d0484b7e5f3bfc5d4a94d5675b319635a175759
b <biro123@ntlworld.com>
2017-11-19 23:21:58 +0000

    PC07 - Added Helicopter, zoom and call-heli functions

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+    private Camera eyes;
+    private float startFov;
+
+	// Use this for initialization
+	void Start () {
+        eyes = GetComponent<Camera>();
+        startFov = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Zoom") )
+        {
+            float zoomAmount = startFov / 1.5f;
+            Zoom(zoomAmount);
+        }
+        else
+        {
+            Zoom(startFov);
+        }
+	}
+
+    void Zoom(float fieldOfView)
+    {
+        eyes.fieldOfView = fieldOfView;
+    }
+
+}
