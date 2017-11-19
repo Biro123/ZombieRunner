cb4c02a245cff2f4ffdfeb7b058391e39d154748
b <biro123@ntlworld.com>
2017-11-19 00:05:29 +0000

    PC06 - Added multiple spawn points and player script. Refined more terrain.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject playerSpawnPoints;
+    public bool respawnNow = false;
+    private Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (respawnNow)
+        {
+            ReSpawn();
+            respawnNow = false;
+        }
+	}
+
+    public void ReSpawn()
+    {
+        // instance 0 is the parent's transform - so ignore.
+        int spawnInstance = Random.Range(1, spawnPoints.Length);
+        transform.position = spawnPoints[spawnInstance].transform.position;
+    }
+}
